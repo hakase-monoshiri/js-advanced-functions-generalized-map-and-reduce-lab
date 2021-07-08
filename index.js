799022bee3b6f51f6f5dd97d9ac8f855e1d3f0f3
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-07-08 18:59:30 -0400

    commit tree fed1ec73b2b46b050bebdf3bc17680a78e5cfbba

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+function map(sourceArray, callbackFn) {
+    let newArray = [];
+
+    for (const e of sourceArray) {
+     newArray.push( 
+         callbackFn(e)
+         ) 
+ }
+ return newArray
+};
+function reduce(sourceArray, callbackFn, startingPoint=0) {
+    let runningTotal = startingPoint;
+
+    for (const e of sourceArray) {
+       
+        runningTotal = callbackFn( e, runningTotal=true) 
+    };
+       
+    return runningTotal
+};
